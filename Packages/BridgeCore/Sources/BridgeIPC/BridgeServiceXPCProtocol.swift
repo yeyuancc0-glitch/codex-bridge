8101a8cba30bfcc369f0672a324d576237ae5cdc
yeyuancc0-glitch <yeyuancc0@gmail.com>
2026-08-27 04:14:25 -0400

    Fix BridgeIPC duplicate and make XPC protocols Windows-compatible

--- Packages/BridgeCore/Sources/BridgeIPC/BridgeServiceXPCProtocol.swift
+++ Packages/BridgeCore/Sources/BridgeIPC/BridgeServiceXPCProtocol.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+#if canImport(Darwin)
 @objc public protocol CodexBridgeServiceXPCProtocol {
   func perform(_ request: Data, withReply reply: @escaping (Data) -> Void)
 }
@@ -7,10 +8,14 @@ import Foundation
 @objc public protocol CodexBridgeTaskStreamListener {
   func push(_ payload: Data)
 }
-
-public enum BridgeServiceIPC {
-  public static let machServiceName = "org.codexbridge.service"
-  public static let launchAgentPlistName = "org.codexbridge.service.plist"
-  public static let schemaVersion = 4
-  public static let maximumMessageBytes = 8 * 1_024 * 1_024
+#else
+public protocol CodexBridgeServiceXPCProtocol {
+  func perform(_ request: Data, withReply reply: @escaping (Data) -> Void)
 }
+
+public protocol CodexBridgeTaskStreamListener {
+  func push(_ payload: Data)
+}
+#endif
+
+
